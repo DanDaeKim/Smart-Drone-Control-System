82f4f0517d88fed3088b781437c2b8615d769184
DanDaeKim <131207269+DanDaeKim@users.noreply.github.com>
2023-04-19 20:41:46 -0700

    Update key.py

--- key.py
+++ key.py
@@ -1,18 +1,27 @@
 from nacl.public import PrivateKey
 
-def message_handler(drone, sender_id, message):
-    print(f"Drone {drone.id} received message from {sender_id}: {message}")
+# Define a message handler function
+def message_handler(drone_id, message):
+    print(f"Message from {drone_id}: {message}")
 
 drone1_key = PrivateKey.generate()
 drone2_key = PrivateKey.generate()
+drone3_key = PrivateKey.generate()
 
 drone_keys = {
     "drone1": drone1_key.public_key,
     "drone2": drone2_key.public_key,
+    "drone3": drone3_key.public_key,
 }
 
 drone1 = Drone("drone1", (0, 0))
 drone2 = Drone("drone2", (0, 0))
+drone3 = Drone("drone3", (0, 0))
 
+# Create DroneCommunication instances for each drone
 drone1_comm = DroneCommunication(drone1, drone1_key, drone_keys, "tcp://*:5555", message_handler)
-drone2
+drone2_comm = DroneCommunication(drone2, drone2_key, drone_keys, "tcp://*:5556", message_handler)
+drone3_comm = DroneCommunication(drone3, drone3_key, drone_keys, "tcp://*:5557", message_handler)
+
+
+
